ecb3507ebc6a265d59eb96cde4f83334f8ebd1fd
Brad Caron <bacaron@iu.edu>
2019-08-24 03:43:33 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -8,15 +8,16 @@ export ITK_GLOBAL_DEFAULT_NUMBER_OF_THREADS=$NCORE
 
 # make output directory
 mkdir output
+mkdir output/output
 
 # set variables
 freesurfer=`jq -r '.freesurfer' config.json`
 
 # copy data from freesurfer to output
-cp -R ${freesurfer}/* ./output/
+cp -R ${freesurfer}/* ./output/output/
 
 # set subjects directory for freesurfer
-SUBJECTS_DIR=`pwd`
+SUBJECTS_DIR=`pwd`/output
 
 # run segmentation
 segmentThalamicNuclei.sh output ${SUBJECTS_DIR}
