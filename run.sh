6712fd3174ca784dccca19d33d48d4e107bf3fb0
Brad Caron <bacaron@iu.edu>
2019-08-24 01:59:50 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -10,9 +10,3 @@ freesurfer=`jq -r '.freesurfer' config.json`
 cp -R ${freesurfer}/* ./output/
 
 segmentThalamicNuclei.sh output ./
-
-# use output.json as product.Json
-#echo "{\"track\": $(cat output.json)}" > product.json
-#
-## clean up
-#rm -rf *.mif *.b ./tmp
