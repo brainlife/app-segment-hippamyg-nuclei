af79ab952ff2d1c5057472103e85a42d06fffa1e
Brad Caron <bacaron@iu.edu>
2019-08-24 03:08:07 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,9 +4,17 @@ set -e
 
 NCORE=8
 
+# make output directory
 mkdir output
+
 # set variables
 freesurfer=`jq -r '.freesurfer' config.json`
+
+# copy data from freesurfer to output
 cp -R ${freesurfer} ./output/
 
-segmentThalamicNuclei.sh output ./
+# set subjects directory for freesurfer
+SUBJECTS_DIR=`pwd`
+
+# run segmentation
+segmentThalamicNuclei.sh output ${SUBJECTS_DIR}
